d6d29818da244dfb807bef9b69bdd4fda78e11a3
Ryan <jaewoopk000@naver.com>
2023-01-26 23:06:47 +0900

    Day 7 Trying to revise all thing because of it should be classified by html,css and js

---
+++ button/OneTouch.js
@@ -0,0 +1,30 @@
+<body>
+    <div class="app">
+        <header>
+            <h1>Color Tap Music</h1>
+            <p>"Music washes away from the soul the dust of everyday life."</p>
+        </header>
+        <div class="visual"></div>
+        <p class="title"></p>
+        <div class="pads">
+            <div class="pad1">
+                <audio class="sound"  src="sound/sample1.mp3"></audio>
+            </div>
+            <div class="pad2">
+                <audio class="sound" src="sound/sample2.mp3"></audio>
+            </div>
+            <div class="pad3">
+                <audio class="sound" src="sound/sample3.mp3"></audio>
+            </div>
+            <div class="pad4">
+                    <audio class="sound" src="sound/sample4.mp3"></audio>
+            </div>
+            <div class="pad5">
+                    <audio class="sound" src="sound/sample5.mp3"></audio>
+            </div>
+            <div class="pad6">
+                    <audio class="sound" src="sound/sample6.mp3"></audio>
+            </div>
+        </div>
+    </div>
+</body>
